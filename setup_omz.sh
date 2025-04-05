849f22902ceaa5e4f4f444b32460d7eb92ca8bbd
Johannes-Flachberger <jo.flachberger@gmail.com>
2025-04-06 01:23:28 +0200

    (fix) removed check for OS in bash script

--- setup_omz.sh
+++ setup_omz.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    sudo apt install zsh
-fi
+sudo apt install zsh
 sudo rm -r ~/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
